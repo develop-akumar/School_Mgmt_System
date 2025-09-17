541adac17edc86b7525ec78e616d9460803ee794
Arvind Chaurasia <develop.akumar@gmail.com>
2025-09-17 12:02:30 +0530

    Created API service File

---
+++ frontend/src/app/services/api/api.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  apiurl: string = environment.apiurl; 
+
+  constructor(private http: HttpClient) {}
+
+  getNotices() {
+    return this.http.get(`${this.apiurl}`);
+  }
+}
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+    apiurl:"http://localhost:3000/api"
+};
